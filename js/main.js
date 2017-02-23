d82012aa311cce210a3ee4edf6a380f8ae354c55
Jaro.js <spyjaro@gmail.com>
2017-02-24 02:43:04 +0300

    mute button added

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
-var vid,playbtn,currentTimeText,durationTimeText;
+var vid,playButton,currentTimeText,durationTimeText,muteButton;
 function initializePlayer(){
 	//set object references
 	vid = document.getElementById('my_video');
-	playbtn = document.getElementById('playpausebtn');
-	seekslider = document.getElementById('seekslider');
+	playButton = document.getElementById('playpausebtn');
+	seekSlider = document.getElementById('seekSlider');
 	currentTimeText = document.getElementById('currentTimeText');
 	durationTimeText = document.getElementById('durationTimeText');
+	muteButton = document.getElementById('muteButton');
 
 	//add event listeners 
-	playbtn.addEventListener('click',playPause,false);
-	seekslider.addEventListener('change',vidSeek,false);
+	playButton.addEventListener('click',playPause,false);
+	seekSlider.addEventListener('change',vidSeek,false);
 	vid.addEventListener('timeupdate',seekTimeUpdate,false);
+	muteButton.addEventListener('click',vidMute,false);
 }
 
 window.onload = initializePlayer;
@@ -18,33 +20,39 @@ window.onload = initializePlayer;
 function playPause() {
 	if(vid.paused){
 		vid.play();
-		playbtn.innerHTML = "&nbsp;&#10073&#10073&nbsp;"
+		playButton.innerHTML = "&nbsp;&#10073&#10073&nbsp;"
 	}else{
 		vid.pause();
-		playbtn.innerHTML = "&#9658"
+		playButton.innerHTML = "&#9658"
 	}
 }
 function vidSeek(){
-	var seekto = vid.duration * (seekslider.value / 100);
-	vid.currentTime = seekto;
+	var seekTo = vid.duration * (seekSlider.value / 100);
+	vid.currentTime = seekTo;
 }
 function seekTimeUpdate() {
 	var newTime = vid.currentTime*(100/vid.duration);
-	seekslider.value = newTime;
+	seekSlider.value = newTime;
 
-	var durationMinutes = Math.floor(vid.currentMinutes / 60);
-	var currentSeconds = Math.floor(vid.currentSeconds - currentMinutes*60);
-	var durationMinutes = Math.floor(vid.durationMinutes / 60);
-	var durationSeconds = Math.floor(vid.durationSeconds - durationMinutes*60);
+	var currentMinutes = Math.floor(vid.currentTime / 60);
+	var currentSeconds = Math.floor(vid.currentTime - currentMinutes*60);
+	var durationMinutes = Math.floor(vid.duration / 60);
+	var durationSeconds = Math.floor(vid.duration - durationMinutes*60);
 
-	if(currentSeconds < 10 ){
-		currentSeconds = "0" + currentSeconds;
+	if(currentSeconds < 10 ){currentSeconds = "0" + currentSeconds;}
+	if(durationSeconds < 10 ){durationSeconds = "0" + durationSeconds;}
+	if(currentMinutes < 10 ){currentMinutes = "0" + currentMinutes;}
+	if(durationMinutes < 10 ){durationMinutes = "0" + durationMinutes;}
+
+	currentTimeText.innerHTML= currentMinutes +":" + currentSeconds;
+	durationTimeText.innerHTML= durationMinutes +":" + durationSeconds;
+}
+function vidMute(){
+	if(vid.muted){
+		vid.play();
+		
+	}else{
+		vid.pause();
+		
 	}
-
-	if(durationSeconds < 10 ){
-		durationSeconds = "0" + durationSeconds;
-	}
-
-	currentTimeText.innerHTML= currentMinutes +" : " + currentSeconds;
-	durationTimeText.innerHTML= durationMinutes +" : " + durationSeconds;
 }
